702845d1772e681125a51d7e271a6dd39ff3bfe6
Yu QIU <yu.qiu@amadeus.com>
2015-05-21 15:47:15 +0200

    add tests for models, run BACKTRACE=1 bin/rake test test/models/article_test.rb to test

--- blog/test/models/article_test.rb
+++ blog/test/models/article_test.rb
@@ -4,4 +4,15 @@ class ArticleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save article without title" do
+    article = Article.new
+    assert_not article.save, "saved the article without a title"
+  end
+
+  #test "should report error" do
+    # some_undefined_variable is not defined elsewhere in the test case
+  #  some_undefined_variable
+  #  assert true
+  #end
 end
