53c07ea3f0837c41973e10cef4c5e7ca00b282b2
Yu QIU <yu.qiu@amadeus.com>
2015-05-22 11:48:49 +0200

    add user model and do some tests, add index to uer email (so delete fixturesos user)

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518135156) do
+ActiveRecord::Schema.define(version: 20150522093021) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -29,5 +29,14 @@ ActiveRecord::Schema.define(version: 20150518135156) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
